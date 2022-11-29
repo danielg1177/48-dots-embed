37e9aa51990265ba6c7803d2e895391eaf148d32
Daniel Gordon <danielgordon19972@gmail.com>
2022-11-29 20:45:16 +0200

    WIP

--- src/main.js
+++ src/main.js
@@ -1,66 +1,19 @@
-import { ping } from './services'
-import { show } from './views/message'
-
-const supportedAPI = ['init', 'message']; // enlist all methods supported by API (e.g. `mw('event', 'user-login');`)
-
+import './views/message.css';
 /**
     The main entry of the application
     */
 function app(window) {
-    console.log('JS-Widget starting');
+    console.log('starting');
 
-    // set default configurations
-    let configurations = {
-        someDefaultConfiguration: false
-    };
+    let container = document.createElement('div');
+    let starting_text = document.createElement('p');
+    starting_text.classList.add('embed_text');
+    starting_text.textContent = "I want to";
+    container.appendChild(starting_text)
 
-    // all methods that were called till now and stored in queue
-    // needs to be called now 
-    let globalObject = window[window['JS-Widget']];
-    let queue = globalObject.q;
-    if (queue) {
-        for (var i = 0; i < queue.length; i++) {
-            if (queue[i][0].toLowerCase() == 'init') {
-                configurations = extendObject(configurations, queue[i][1]);
-                console.log('JS-Widget started', configurations);
-            }
-            else
-                apiHandler(queue[i][0], queue[i][1]);
-        }
-    }
-
-    // override temporary (until the app loaded) handler
-    // for widget's API calls
-    globalObject = apiHandler;
-    globalObject.configurations = configurations;
-}
-
-/**
-    Method that handles all API calls
-    */
-function apiHandler(api, params) {
-    if (!api) throw Error('API method required');
-    api = api.toLowerCase();
-
-    if (supportedAPI.indexOf(api) === -1) throw Error(`Method ${api} is not supported`);
-
-    console.log(`Handling API call ${api}`, params);
-
-    switch (api) {
-        // TODO: add API implementation
-        case 'message':
-            show(params);
-            break;
-        default:
-            console.warn(`No handler defined for ${api}`);
-    }
-}
-
-function extendObject(a, b) {
-    for (var key in b)
-        if (b.hasOwnProperty(key))
-            a[key] = b[key];
-    return a;
+    // append elements to body
+    let entrance = document.getElementById('embed_entrance');
+    entrance.appendChild(container);
 }
 
 app(window);
